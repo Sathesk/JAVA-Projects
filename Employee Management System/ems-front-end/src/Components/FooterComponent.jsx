caa957afb493094fb5cc22a6492ab016270ca411
Chandramohan Satheskanth <131255616+Sathesk@users.noreply.github.com>
2025-03-10 00:58:18 +0530

    Add files via upload

---
+++ Employee Management System/ems-front-end/src/Components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>@All rights reserved by satheskanth</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
